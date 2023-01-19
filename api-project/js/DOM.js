52edf1eca3cf9ccab6e67e6611cc2359859e2fd9
JunxiC <chenjon09@gmail.com>
2023-01-19 10:16:45 -0500

    api commit 10

--- api-project/js/DOM.js
+++ api-project/js/DOM.js
@@ -2,5 +2,6 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   button2: document.getElementById("button2"),
   box: document.getElementById("container"),
+  character: document.querySelector(`#character`),
 };
 export { DOMSelectors };
--- api-project/js/index.js
+++ api-project/js/index.js
@@ -5,47 +5,44 @@ import { DOMSelectors } from "./DOM";
 
 // (If you open this in a browser, you will get raw object data.)
 const apiEntry = "https://api.genshin.dev/characters/";
-const character = document.querySelector("#character").value;
+const character = document.querySelector(`#character`).value;
 
 const realAPI = `${apiEntry}${character}`;
 console.log(realAPI);
 
-// fetch is a function (that you've seen previously) that can retrieve
-// data from an api entry point.
-console.log(fetch(apiEntry));
+DOMSelectors.button2.addEventListener("click", function () {
+  let character = DOMSelectors.character.value;
+  DOMSelectors.character.value = "";
 
-// fetch() returns a "response", which we must convert into a object json format
-fetch(apiEntry)
-  .then((response) => response.json()) // use the `.json()` method
-  .then((data) => console.log(data)); // `.json()` is also async, chain another `.then()` to log the object
+  const realAPI = `${apiEntry}${character}`;
 
-// let's turn this to an async/await function
-async function fetchData(apiEntry) {
-  try {
-    const response = await fetch(apiEntry);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (err) {
-    console.error(err);
+  fetch(apiEntry)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+  async function fetchData(apiEntry) {
+    try {
+      const response = await fetch(apiEntry);
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
   }
-}
-fetchData(apiEntry);
+  fetchData(apiEntry);
 
-// paired with DOM selectors, you can display dynamic data onto your HTML!
-const apiResponseDOM = document.getElementById("api-response");
-const putQuoteInHTML = async () => {
-  // defining an async arrow function
-  const quote = await fetchData(realAPI);
-  apiResponseDOM.innerHTML = `<div class="inner" data-aos="flip-up">
+  const apiResponseDOM = document.getElementById("api-response");
+  const putQuoteInHTML = async () => {
+    const quote = await fetchData(realAPI);
+    apiResponseDOM.innerHTML = `<div class="inner" data-aos="flip-up">
   <p>${quote.name}</p>
+  <p>${quote.title}</p>
   <p>Type: ${quote.vision}</p>
+  <p>Weapon: ${quote.weapon}</p>
+  <p>Nation: ${quote.nation}</p>
   <p>${quote.description}</p>
 </div>`;
-};
-putQuoteInHTML();
-
-DOMSelectors.button2.addEventListener("click", function (event) {
-  clearInput();
-  event.preventDefault();
+  };
+  putQuoteInHTML();
 });
